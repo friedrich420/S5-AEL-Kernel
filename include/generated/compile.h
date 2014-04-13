5411226175a519e9f46eca0e073c74c438b1067d
friedrich420 <kelpidorou@yahoo.com>
2014-04-13 11:35:49 +0300

    SM-G900F_KK_Opensource_Update1

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 SMP PREEMPT Wed Apr 9 11:03:06 KST 2014"
+#define LINUX_COMPILE_BY "omsgsm"
+#define LINUX_COMPILE_HOST "sy918-desktop"
+#define LINUX_COMPILER "gcc version 4.7 (GCC) "
